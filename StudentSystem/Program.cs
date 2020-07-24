23d165ed21fe55e38a5d20b07c6b76b2b86c2381
David Miko <ph4nt0mgh0sT17@gmail.com>
2020-07-24 16:16:11 +0200

    Add project files.

---
+++ StudentSystem/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace StudentSystem
+{
+    internal class Program
+    {
+        /// <summary>
+        /// The main entry of the application.
+        /// </summary>
+        private static void Main()
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
